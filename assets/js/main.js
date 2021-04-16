d51eb4da63780ab65655038a745c1d87532f3cdb
alkhioz <alemendoza0197@gmail.com>
2021-04-16 12:36:52 -0500

    make code more clear

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,10 @@ var image_form = document.querySelector("#addImage");
 var activeNode;
 
 let nodeFocus  = (target) =>  {
-    let resizableContainer = target.parentElement;
+    if(activeNode){
+        nodeLostFocus();
+    }
+    let resizableContainer = target.closest(".resizableContainer");
     let resizableContainerCurrentWidth = resizableContainer.offsetWidth+'px';
     resizableContainer.classList.add("resizableContainerResize");
     resizableContainer.classList.remove("resizableContainerFix");
@@ -23,7 +26,8 @@ let nodeLostFocus = () => {
 window.onclick = function(e) {
     let event = e || window.event;
     let target = event.target || event.srcElement;
-    if(target.classList.contains("responsiveImage")){
+
+    if(target.classList.contains("responsiveImage") || target.classList.contains("responsiveVideoOverlay")){
         nodeFocus(target);
     }else{
         if(activeNode){
@@ -33,9 +37,16 @@ window.onclick = function(e) {
     }
 }
 
+let createResizableContainer = (newChild) =>{
+    let templateContainer = document.querySelector("#containerTemplate");
+    let clone = templateContainer.content.cloneNode(true);
+    let resizableContainer = clone.querySelector(".resizableContainer");
+    resizableContainer.appendChild(newChild);
+    document.querySelector("body").appendChild(clone);
+}
+
 if(document.contains(image_form)){
     image_form.addEventListener('submit', ()=>{
-
         event.preventDefault();
         let imageSrc = document.querySelector("#srcImage").value;
         let imageAlt = document.querySelector("#altImage").value;
@@ -44,27 +55,21 @@ if(document.contains(image_form)){
         let image = clone.querySelector("img");
         image.src = imageSrc;
         image.alt = imageAlt;
-        document.querySelector("body").appendChild(clone);
-
+        createResizableContainer(clone);
     });
 }
 
 //video
 var video_form = document.querySelector("#addVideo");
-var activeVideoNode;
 
 if(document.contains(video_form)){
     video_form.addEventListener('submit', ()=>{
-
         event.preventDefault();
         let videoSrc = document.querySelector("#srcVideo").value;
-        
-            let templateVideoToInsertIframe = document.querySelector("#videoTemplate");
-            let clone = templateVideoToInsertIframe.content.cloneNode(true);
-            let video = clone.querySelector("iframe");
-            video.src = videoSrc;
-            document.querySelector("body").appendChild(clone);
-        
-
+        let templateVideoToInsertIframe = document.querySelector("#videoTemplate");
+        let clone = templateVideoToInsertIframe.content.cloneNode(true);
+        let video = clone.querySelector("iframe");
+        video.src = videoSrc;
+        createResizableContainer(clone);
     });
 }
